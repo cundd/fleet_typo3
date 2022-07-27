43333dca7a2583c7406c1154e6b2cfec7b2bd0d3
Daniel Corn <daniel@iresults.li>
2022-07-27 11:24:10 +0200

    Add support for TYPO3 v11

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '0.2.2-dev',
     'constraints' => [
         'depends' => [
-            'typo3' => '6.2.0-10.4.99',
+            'typo3' => '6.2.0-11.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
